595ac0ab19e8228a10f8acf0a190035f1a0387b5
srikanth Karra <srikanthreddy376@gmail.com>
2019-04-27 17:36:04 +0200

    added new test cases

--- Examples.js
+++ Examples.js
@@ -1,4 +1,7 @@
 const functions = {
-  add: (a, b) => a + b
+  add: (a, b) => a + b,
+  checkValue: () => null,
+  greaterValue: (a, b) => a < b,
+  Square: a => a * a
 };
 module.exports = functions;
--- Examples.test.js
+++ Examples.test.js
@@ -1,5 +1,15 @@
 const functions = require("./Examples");
-
+//is equality test
 test("It should return sum of 2 + 2 ", () => {
   expect(functions.add(2, 2)).toBe(4);
+});
+// is string test
+test("It should return the null", () => {
+  expect(functions.checkValue()).toBeNull();
+});
+test("It should return  a <  b", () => {
+  expect(functions.greaterValue(10, 4));
+});
+test("It should return a 625", () => {
+  expect(functions.Square(25)).toBe(625);
 });
